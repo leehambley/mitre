476a83bdc940a1fe6ab37fa2c79acb8406a55f91
Daniel Schmidt <danielmschmidt92@gmail.com>
2020-12-29 18:42:11 +0100

    format using cargo fmt

--- src/runner.rs
+++ src/runner.rs
@@ -2,13 +2,13 @@ pub mod mariadb;
 use crate::config::Configuration;
 
 pub trait Runner {
-  type Error;
+    type Error;
 
-  fn new(config: &Configuration) -> Result<Self, Self::Error>
-  where
-    Self: Sized;
+    fn new(config: &Configuration) -> Result<Self, Self::Error>
+    where
+        Self: Sized;
 
-  fn bootstrap(&mut self) -> Result<(), Self::Error>
-  where
-    Self: Sized;
+    fn bootstrap(&mut self) -> Result<(), Self::Error>
+    where
+        Self: Sized;
 }
