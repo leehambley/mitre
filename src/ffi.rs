6cd2037059e501c928b25bb1f9d226dcea27be64
Daniel Schmidt <danielmschmidt92@gmail.com>
2021-03-08 21:40:33 +0100

    implement basic logger

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,13 +1,72 @@
 use std::ffi::{c_void, CString};
 use std::os::raw::c_char;
-pub fn report_status(msg: String) {}
+
+type LoggingFunction = unsafe extern "C" fn(*mut c_char, *mut c_char) -> c_void;
+unsafe extern "C" fn default_logger(_lvl: *mut c_char, _msg: *mut c_char) -> c_void {
+    panic!("FFI logger not configured");
+}
 
 // https://rust-lang.github.io/unsafe-code-guidelines/layout/function-pointers.html
 
+static mut LOGGER: LoggingFunction = default_logger;
+
+/// https://docs.rs/flexi_logger/0.17.1/flexi_logger/struct.Record.html
+struct CLIAndFFILogger;
+impl log::Log for CLIAndFFILogger {
+    fn enabled(&self, _metadata: &log::Metadata) -> bool {
+        println!("in enabled");
+        true
+    }
+    fn log(&self, record: &log::Record) {
+        println!("Ho");
+        // if !self.enabled(record.metadata()) {
+        //     return;
+        // }
+
+        let r_msg = format!(
+            "{}:{} -- {}",
+            record.level(),
+            record.target(),
+            record.args()
+        );
+        let ffi_lvl = CString::new(format!("{}", record.level()))
+            .expect("lvl should be convertable to CString");
+        let ffi_msg =
+            CString::new(format!("{}", record.args())).expect("msg should convertable to CString");
+        println!("{}", r_msg);
+        unsafe {
+            LOGGER(ffi_lvl.into_raw(), ffi_msg.into_raw());
+        }
+    }
+    fn flush(&self) {
+        println!("in flush");
+    }
+}
+
 #[no_mangle]
-pub unsafe fn set_report_status_callback(func: unsafe extern "C" fn(*mut c_char) -> c_void) {
-    let str = CString::new("Hello FFI").expect("boom:reason");
-    func(str.into_raw());
+pub unsafe fn set_log_level(lvl: *mut c_char) {
+    let _level = CString::from_raw(lvl)
+        .to_str()
+        .expect("level should be set");
+
+    // TODO: implement this
+    log::set_max_level(log::LevelFilter::Debug);
+}
+
+#[no_mangle]
+pub unsafe fn set_logger_fn(func: LoggingFunction) {
+    // https://docs.rs/env_logger/0.8.3/src/env_logger/lib.rs.html#799-802
+    LOGGER = func;
+    let _ = log::set_boxed_logger(Box::new(CLIAndFFILogger {}));
+    log!(log::Level::Error, "Received errors");
+    println!("Hey")
+}
+
+#[no_mangle]
+pub unsafe fn do_work() {
+    for x in 0..10 {
+        info!("Iteration Number: {}", x);
+    }
 }
 
 // #[no_mangle]
--- test_node.js
+++ test_node.js
@@ -1,16 +1,28 @@
 var ffi = require("ffi-napi");
 var ref = require("ref-napi");
+var winston = require("winston");
+
+const logger = winston.createLogger({
+  transports: [new winston.transports.Console()],
+});
 var StructType = require("ref-struct-di")(ref);
 var ArrayType = require("ref-array-di")(ref);
 
 var libmitre = ffi.Library("./target/debug/libmitre.dylib", {
-  set_report_status_callback: ["void", ["pointer"]],
+  set_logger_fn: ["void", ["pointer"]],
+  do_work: ["void", []],
 });
 
-var callback = ffi.Callback("void", ["string"], function (msg) {
-  console.log("This came from Rust?!?!?!: ", msg);
-});
+var callback = ffi.Callback(
+  "void",
+  ["string", "string"],
+  function (level, message) {
+    console.log(message);
+    winston.log({ level, message });
+  }
+);
 
 console.log("registering the callback");
 
-libmitre.set_report_status_callback(callback);
+libmitre.set_logger_fn(callback);
+libmitre.do_work();
